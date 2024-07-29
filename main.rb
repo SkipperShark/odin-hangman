6b17129013f731b7fdfb338c1dd8ddb935fa7afe
skipperShark <sharmanfaridz@gmail.com>
2024-07-29 09:24:03 +0800

    implement file not found error handling

--- main.rb
+++ main.rb
@@ -1,2 +1,9 @@
-words = File.open("words.txt")
-puts words.readlines.size
+filename = "words.txt"
+
+raise StandardError, "words file not found!" unless File.exist? filename
+
+words = File.open(filename, "r").readlines
+puts words.length
+
+filtered_words = words.filter { |word| word.length < 12 && word.length > 5}
+puts filtered_words.length
